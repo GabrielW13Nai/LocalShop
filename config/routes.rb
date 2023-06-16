a2c3484649d96a726aca0921ea5a0a3fb71c2a21
Gabriel Wambugu <gabriel.wambugu@student.moringaschool.com>
2023-06-16 19:47:52 +0300

    admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :item_admins
+  resources :items
+  resources :admins
+  resources :merchants
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
