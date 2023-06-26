55f1d9cc710a1db2d24873c7d593da1493f2a8e4
jacinta mwangi <jacinta.mwangi@student@moringaschool.com>
2023-06-26 21:37:44 +0300

    modified

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_25_185639) do
     t.string "alias"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest"
   end
 
   create_table "role_permissions", force: :cascade do |t|
