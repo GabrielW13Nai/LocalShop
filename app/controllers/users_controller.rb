9037cea72ba4e92fc2dddbca064d1f3b5790ddf5
jacinta mwangi <jacinta.mwangi@student@moringaschool.com>
2023-07-02 09:52:46 +0300

    active model serializer gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,19 @@
 class UsersController < ApplicationController
     def create
-        @user = User.create(user_params)
-        if @user.valid?
+        @user = User.new(user_params)
+
+        if @user.save
+            token = encode_token({user_id: @user.id})
+            render json: {user: @user, token: token}, status: :ok
+        else
+            render json: { error: 'Failed to create user', errors: @user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def login 
+        @user = User.find_by(name: user_params[:name])
+        
+        if @user&.authenticate(user_params[:password])
             token = encode_token({user_id: @user.id})
             render json: {user: @user, token: token}, status: :ok
         else
@@ -11,7 +23,7 @@ class UsersController < ApplicationController
 
     private
     def user_params
-        params.require(:user).permit(:name, :email, :password)
-    end
+        params.require(:user).permit(:name, :email, :password, :password_confirmation, :role)
+end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:create]
+  post "/login", to: "users#login"
   
   
 end
