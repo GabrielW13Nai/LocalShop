053229c542dd7debd67aa754694832c45ca700c0
jacinta mwangi <jacinta.mwangi@student@moringaschool.com>
2023-07-03 11:22:10 +0300

    did a migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_01_121634) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_03_081231) do
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.string "quantity"
@@ -22,6 +22,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_01_121634) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "image_url"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +46,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_01_121634) do
   end
 
   add_foreign_key "items", "users"
+  add_foreign_key "profiles", "users"
 end
