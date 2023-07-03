6f8b2e5a91cf3db61098ea4655a858061481923c
jacinta mwangi <jacinta.mwangi@student@moringaschool.com>
2023-07-03 12:00:40 +0300

    updated routes for profile

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,10 @@ Rails.application.routes.draw do
   resources :books, only: [:index, :show, :create, :update, :destroy] do
     post 'upload', on: :collection
   end
+
+  get '/profiles/:id', to: 'profiles#show'
+  patch '/profiles/:id', to: 'profiles#update'
+  get '/profiles/:id/edit', to: 'profiles#edit', as: 'edit_profile'
+
   
 end
