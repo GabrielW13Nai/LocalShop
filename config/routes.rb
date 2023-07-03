7ce34f0bad92e8a811cd80ce5940937281da41f0
jacinta mwangi <jacinta.mwangi@student@moringaschool.com>
2023-07-03 11:30:47 +0300

    minor changes to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :profiles
   resources :users, only: [:create]
   post "/login", to: "users#login"
   
+  resources :profiles
+  resources :books, only: [:index, :show, :create, :update, :destroy] do
+    post 'upload', on: :collection
+  end
   
 end
