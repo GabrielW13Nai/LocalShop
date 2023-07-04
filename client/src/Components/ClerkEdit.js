3ce6e5ab8f36f51801de6d8a297c4ef0f437ebf6
Gabriel Wambugu <gabriel.wambugu@student.moringaschool.com>
2023-07-04 08:51:22 +0300

    changes to admin

--- client/src/Components/ClerkEdit.js
+++ client/src/Components/ClerkEdit.js
@@ -1,71 +1,26 @@
-import{ React, useState } from "react";
+import{ React } from "react";
+// import{ useState } from "react";
 import { Link } from "react-router-dom";
+import { Table } from "./Table";
 
 
 function ClerkEdit(){
-    // const[name, setName]= useState(" ")
-    // const[admin_id, setAdminId]= useState(" ")
-
-    const[value, setValue] = useState();
-
-    const handleChange = (e) => {
-        setValue(e.target.value)
-    }
-    const onKeyDown = (e) => {
-        if(e.key === "Enter" || e.key === "Escape"){
-            e.target.blur();
-        }
-    }
-
-    const onBlur = (e) => {
-        setValue(e.target.value)
-    }
-
-
-
-
-
-    // const clerk = {
-    //     name,
-    //     admin_id
-    // }
-
-    // useEffect(()=> {
-    //     fetch('/admins')
-    //     .then(res=> res.json())
-    //     .then((clerk=> setAdmin(clerk)))
-    // }, [])
-
-    // function handleEdit(e){
-    //     e.preventDefault();
-    //     fetch('/clerks',{
-    //         method: "PATCH",
-    //         headers: {"Content-Type":"application/json"},
-    //         body: JSON.stringify(clerk)})
-    //     .then(res=> res.json())
-    //     .then(clerk=> console.log(clerk))
-    // }
 
     return (
         <>
-
-            <div className="clerk-2">
+         <div className="clerk-2">
             <Link to="/clerkinfo"><button className="clerk-btn-back"> &larr; BACK </button></Link><br></br>
-            </div>
+        </div>
 
-            <textarea
-            rows={5}
-            columns={6}
-            type="text"
-            className="input"
-            aria-label="Field name"
-            onChange={handleChange}
-            onKeyDown={onKeyDown}
-            onBlur={onBlur}
-            value={value}
-            />
+        <Table />
+
         </>
+
     )
 
 }
+
 export default ClerkEdit;
+
+
+
---
+++ client/src/Components/Table.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const Table = () => {
+  return (
+    <div className='tableclerks'>
+        <thead>
+            <tr>
+                <th>Clerk Name</th>
+                <th>Email</th>
+                <th>Phone Number</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td> Clerk Name</td>
+            </tr>
+        </tbody>
+
+    </div>
+  )
+}
