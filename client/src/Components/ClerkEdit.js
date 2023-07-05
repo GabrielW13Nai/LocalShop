ba95a1d027afbb2bb8eb756fda7d87964c466c0c
Gabriel Wambugu <gabriel.wambugu@student.moringaschool.com>
2023-07-05 08:57:16 +0300

    admin

--- client/src/Components/ClerkEdit.js
+++ client/src/Components/ClerkEdit.js
@@ -1,10 +1,52 @@
 import{ React } from "react";
-// import{ useState } from "react";
+import{ useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Table } from "./Table";
+import { Modal } from './Modal'
+
 
 
 function ClerkEdit(){
+
+    const[modal, setModal] = useState(false)
+    const[clerkDelete, setClerkDelete]= useState([])
+    const[rowEdit, setRowEdit]= useState(null)
+
+
+    useEffect(()=> {
+        fetch('/users')
+        .then(res=> res.json())
+        .then((user)=> {
+            setClerkDelete(user);
+            })
+    }
+    , [])
+
+    function handleEdit(id){
+        fetch('/users',{
+            method: "PATCH"})
+            .then(res => res.json())
+            .then(id=>console.log(id))
+
+        setRowEdit(id);
+        setModal(true);
+    }
+
+
+    function handleDelete(id){
+
+        const choice = window.confirm("Are you sure you want to remove this clerk from the system?")
+        if (choice){
+            fetch(`/users/${id}`,{method: "DELETE",})
+            .then(res=> res.json())
+            .then(()=> {
+                const newClerk = clerkDelete.filter(clerk => clerk.id !== id)
+                setClerkDelete(newClerk);
+                })
+                window.location.reload(true)
+            }
+
+    }
 
     return (
         <>
@@ -12,7 +54,11 @@ function ClerkEdit(){
             <Link to="/clerkinfo"><button className="clerk-btn-back"> &larr; BACK </button></Link><br></br>
         </div>
 
-        <Table />
+        <Table DeleteRow={handleDelete} editRow={handleEdit}/>
+        {modal&& <Modal closeModal={()=>setModal(false)}
+
+        defaultValue={rowEdit !==null}/>}
+        <button className="btn-primary" onClick={()=> setModal(true)}>Open</button>
 
         </>
 
---
+++ client/src/Components/Modal.js
@@ -0,0 +1,28 @@
+import React from 'react'
+// import { IoSaveSharp} from 'react-icons/io5'
+
+export const Modal = ( {closeModal}) => {
+  return (
+    <div className='modal-container' onClick={(e)=>{
+        if (e.target.className === "modal-container") closeModal()}}>
+        <div className='modal'>
+            <form>
+                <div className='form-edit'>
+                    <label htmlFor='Clerk Name'>Clerk Name</label>
+                    <input placeholder='Name' />
+                </div>
+                <div className='form-edit'>
+                    <label htmlFor='Email'>Clerk Email</label>
+                    <input placeholder='Email Address' />
+                </div>
+                <div className='form-edit'>
+                    <label htmlFor='Phone Number'>Phone Number</label>
+                    <input placeholder='Phone' />
+                </div>
+                <button type='submit' className='btn'>Submit</button>
+
+            </form>
+        </div>
+    </div>
+  )
+}
