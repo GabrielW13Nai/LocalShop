38e724ffe0a652842929b758a0c286ed72229a8d
Gabriel Wambugu <gabriel.wambugu@student.moringaschool.com>
2023-07-05 08:57:16 +0300

    admin

--- client/src/Components/Table.js
+++ client/src/Components/Table.js
@@ -1,22 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { BsFillTrashFill, BsFillPencilFill } from 'react-icons/bs'
 
-export const Table = () => {
+
+
+export const Table = ( {setModal, DeleteRow, editRow} ) => {
+    const [users, setUsers] = useState([])
+
+
+
+useEffect(()=>{
+    fetch('/users')
+    .then(res=> res.json())
+    .then(user=> setUsers(user))
+},[])
   return (
-    <div className='tableclerks'>
-        <thead>
-            <tr>
+    <>
+        <div className='tableclerks'>
+        <thead >
+            <tr className='label-table'>
                 <th>Clerk Name</th>
                 <th>Email</th>
                 <th>Phone Number</th>
-                <th>Email</th>
+                <th>Role</th>
+                <th className='clerk-img-tbl'>Image</th>
+                <th>Actions</th>
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td> Clerk Name</td>
-            </tr>
+            {users.map(user=>{
+                        if(user.role.name !== 'Clerk') return null;
+                            return(
+                        <tr>
+                                <>
+                                <td> {user.name}</td>
+                                <td> {user.email}</td>
+                                <td> {user.phone_number}</td>
+                                <td> {user.role.name}</td>
+                                <td className='clerk-img-tbl'> {user.user_image}</td>
+                                <td>
+                            <span className='actions'>
+                                <BsFillTrashFill className='trash' onClick={()=>DeleteRow(user.id)}/>
+                                <BsFillPencilFill onClick={()=> editRow(user.id)}/>
+                            </span>
+                        </td>
+                                </>
+
+            </tr>)})}
+
+
         </tbody>
 
     </div>
+
+
+    </>
   )
 }
