6c0651f4a4ace727f5c8f837026587056de83c4a
Gabriel Wambugu <gabriel.wambugu@student.moringaschool.com>
2023-07-07 14:09:34 +0300

    admin

--- client/src/Components/ProductInfo.js
+++ client/src/Components/ProductInfo.js
@@ -5,11 +5,21 @@ function ProductInfo(){
     return (
         <>
         <div className="product-information">
+          <div onClick={handleAdd} className="icons">
+            <img className="img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9t-km7l-fz4xsYz_o7cK_2hWgJ1puvsI3Pg&usqp=CAU" />
+             <p>ADD PRODUCT</p>
+          </div>
+          <div onClick={handleView} className="icons">
+            <img className="img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvNddxuJAgTkl_DC8LzJSeh44dIlECzQc-EA&usqp=CAU" />
+            <p>VIEW PRODUCT</p>
+          </div>
         </div>
-        <h3>Product Information</h3>
-        </>
-    )
+      </>)
 
+const ProductInfo = () => {
+  return (
+    <div>ProductInfo</div>
+  )
 }
 
 export default ProductInfo;
