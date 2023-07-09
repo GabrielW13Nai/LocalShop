711905e1988dee0e77ce62e074ce88a611eaa107
Gabriel Wambugu <gabriel.wambugu@student.moringaschool.com>
2023-07-09 19:57:45 +0300

    Product edit

--- client/src/Components/ProductInfo.js
+++ client/src/Components/ProductInfo.js
@@ -2,8 +2,19 @@
 import { useHistory } from "react-router-dom";
 
 function ProductInfo(){
+
+    const history = useHistory();
+
+    function handleView(){
+      history.push("/viewproducts")
+    }
+
+    function handleAdd(){
+      history.push("/addproducts")
+    }
+
     return (
-        <>
+      <>
         <div className="product-information">
           <div onClick={handleAdd} className="icons">
             <img className="img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9t-km7l-fz4xsYz_o7cK_2hWgJ1puvsI3Pg&usqp=CAU" alt="not found"/>
