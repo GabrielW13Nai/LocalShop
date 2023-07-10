ac15884ef949dc4959ab1e88e0015cc038964d2d
jacinta mwangi <jacinta.mwangi@student@moringaschool.com>
2023-07-10 20:26:27 +0300

    minor changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,24 +53,22 @@ class UsersController < ApplicationController
   
   private
 
-  def create_user
-    @user = User.create(user_params)
-    if @user.valid?
-      token = encode_token({ user_id: @user.id })
-      render json: { user: UserSerializer.new(@user), jwt: token }, status: :created
-    else
-      render json: { error: 'Failed to create user', errors: @user.errors.full_messages }, status: :unprocessable_entity
-    end
-  end 
+  #def create_user
+   # @user = User.create(user_params)
+    #if @user.valid?
+     # token = encode_token({ user_id: @user.id })
+      #render json: { user: UserSerializer.new(@user), jwt: token }, status: :created
+   # else
+      #render json: { error: 'Failed to create user', errors: @user.errors.full_messages }, status: :unprocessable_entity
+    #end
+  #end 
   
   def send_activation_email_to_admin(admin)
     UserMailer.with(user: admin).account_activation.deliver_now
   end
   
-
-    private
-    def user_params
-        params.require(:user).permit(:name, :email, :password, :role)
-    end
+   def user_params
+          params.require(:user).permit(:name, :email, :password, :role)
+      end
 
 end
