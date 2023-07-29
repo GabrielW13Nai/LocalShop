90967a48d37f9374f0166042aefec304ea99cb48
Gabriel Wambugu <gabriel.wambugu@student.moringaschool.com>
2023-07-29 12:01:46 +0300

    update

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Login from './Pages/Login';
+// import Login from './Pages/Login';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
-import { Switch, Route } from 'react-router-dom';
+// import { Switch, Route } from 'react-router-dom';
 
-import SignUp from './Pages/SignUp';
+// import SignUp from './Pages/SignUp';
 import UserContextProvider from './Components/UserContext';
 
 
@@ -22,15 +22,15 @@ root.render(
       <App />
     </UserContextProvider>
 
-
-    <Switch>
+{/*
+    <Switch> */}
       {/* <Route exact path='/admin'><Admin /></Route> */}
       {/* <Route exact path='/dashboard'><Dashboard /></Route> */}
 
-      <Route exact path='/signup'><SignUp /></Route>
-      <Route exact path='/login'><Login /></Route>
+      {/* <Route exact path='/signup'><SignUp /></Route>
+      <Route exact path='/login'><Login /></Route> */}
       {/* <Route exact path='/'><SignUp /></Route> */}
-    </Switch>
+    {/* </Switch> */}
 
   </React.StrictMode>
   </BrowserRouter>
